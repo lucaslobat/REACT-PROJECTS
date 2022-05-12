a19e68eed8155f830bd0a2d2214d11ff1ce33e5e
Lucas Lobato Botelho <criptoryu@gmail.com>
2022-05-12 21:23:21 +0200

    Project concluded, added aditional state and working with forms

--- meme-generator/src/components/Form.js
+++ meme-generator/src/components/Form.js
@@ -3,25 +3,35 @@ import { useState } from "react";
 import data from "./data";
 
 function Form() {
-  //Inicializa el estado con un objeto
-  const [memeObject, setMemeUrl] = useState({
+  //Inicializa el estado con un objeto de 3 propiedades
+  const [memeObject, setMemeObject] = useState({
     topText: "",
     bottomText: "",
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
 
-  // Estado con el array de objetos de meme.
-  const [allMemeImages, setAllMemeImages] = useState(data.data.memes);
+  // Estado con el array de objetos de meme recibido del import de "data".
+  const [allMemeImages] = useState(data.data.memes);
 
   function handleButtonClick(e) {
     e.preventDefault();
     //Genera numero aleatório de 0 a 100;
     const randomNum = Math.floor(Math.random() * 100);
 
-    /*Utiliza la función de estado para cambiar el valor de memeURL
-    con el valor del randomNum pasado a la variable memes*/
-    setMemeUrl((prevState) => {
+    /*Utiliza la función de estado para cambiar el valor de memeObject;
+    Lo que hace es usar spread en sus propiedades y luego reasignar
+    el valor de la propiedad randomImage al valor de la url de un
+    elemento random del array allMemeImages.*/
+    setMemeObject((prevState) => {
       return { ...prevState, randomImage: allMemeImages[randomNum].url };
+    });
+  }
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+
+    setMemeObject((prevState) => {
+      return { ...prevState, [name]: value };
     });
   }
 
@@ -33,12 +43,19 @@ function Form() {
             className="input-text"
             type="text"
             placeholder="Top text"
-          ></input>
+            name="topText"
+            onChange={handleChange}
+            value={memeObject.topText}
+          />
+
           <input
             className="input-text"
             type="text"
             placeholder="Bottom text"
-          ></input>
+            name="bottomText"
+            onChange={handleChange}
+            value={memeObject.bottomText}
+          />
         </div>
 
         <button onClick={handleButtonClick} className="form-button">
@@ -49,11 +66,9 @@ function Form() {
         </button>
       </form>
       <div className="flex image-container">
-        <img
-          alt="meme"
-          className="meme-image"
-          src={memeObject.randomImage}
-        ></img>
+        <img alt="meme" className="meme-image" src={memeObject.randomImage} />
+        <h1 className="meme--text top">{memeObject.topText}</h1>
+        <h1 className="meme--text bottom">{memeObject.bottomText}</h1>
       </div>
     </>
   );
