69bd10908966cbaf30c892feae45c542e62a01cc
Lucas Lobato Botelho <criptoryu@gmail.com>
2022-07-26 15:07:49 +0200

    Basal Metabolism Rate is available now on user information and, some new styles addesd

--- budiet-app/src/components/UserInformation.js
+++ budiet-app/src/components/UserInformation.js
@@ -6,6 +6,19 @@ import Container from "../ui/Container";
 import Button from "../ui/Button";
 
 function UserInformation(props) {
+  let BMR;
+
+  props.userData.gender === "Male"
+    ? (BMR =
+        66.473 +
+        13.7516 * props.userData.weight +
+        5.0033 * props.userData.height -
+        6.755 * props.userData.age)
+    : (BMR =
+        655.0955 +
+        9.5634 * props.userData.weight +
+        (1.8449 * props.userData.height - 4.6756 * props.userData.age));
+
   return (
     <Container className="flex user-information-card">
       <div className="flex user-information-header">
@@ -18,14 +31,24 @@ function UserInformation(props) {
         </Button>
       </div>
 
-      <div className="flex user-information-container">
-        <img
-          className="user-profile-image"
-          src={userProfile}
-          alt="user-profile-avatar"
-        />
-        <div className="user-caloric-info">
-          <span>Basal Metabolism</span>
+      <div className="flex diet-information-container">
+        <div className="flex user-information">
+          <div className="flex user-avatar-name">
+            <img
+              className="user-profile-image"
+              src={userProfile}
+              alt="user-profile-avatar"
+            />
+            <span className="user-name">{props.userData.name}</span>
+          </div>
+          <div className="flex user-body-information">
+            <span>Age - {props.userData.age}</span>
+            <span>Weight - {props.userData.weight}kg</span>
+            <span>Height - {props.userData.height}cm</span>
+          </div>
+        </div>
+        <div className="basal-metabolic-rate">
+          <span>{BMR.toFixed(1)} kcal/day</span>
         </div>
       </div>
     </Container>
