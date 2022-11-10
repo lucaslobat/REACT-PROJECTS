383cffb4242798b66486fccc8668bfdc2767586c
Lucas Lobato Botelho <criptoryu@gmail.com>
2022-11-10 23:29:37 +0100

    Started using basic State hook

--- react-review/src/components/ItemDeCompra.jsx
+++ react-review/src/components/ItemDeCompra.jsx
@@ -1,16 +1,22 @@
+import { useState } from "react";
+
 import "./ItemDeCompra.css";
 
 function ItemDeCompra(props) {
-  const changeTituloHandler = () => {
-    console.log("Click!");
+  const [importe, setImporte] = useState(props.importe);
+
+  const cambiarImporteHandler = () => {
+    setImporte((oldValue) => {
+      return oldValue + 1;
+    });
   };
 
   return (
     <div className="item-de-compra">
       <div className="aligned">{props.fecha}</div>
       <div className="aligned">{props.descripcion}</div>
-      <div className="aligned">{props.importe}</div>
-      <button onClick={changeTituloHandler}>Cambiar título</button>
+      <div className="aligned">{importe}</div>
+      <button onClick={cambiarImporteHandler}>Cambiar importe</button>
     </div>
   );
 }
