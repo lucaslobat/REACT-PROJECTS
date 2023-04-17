10a8b97a60023007fe6458203f953ff66b454257
Lucas Lobato Botelho <criptoryu@gmail.com>
2023-04-17 10:38:17 +0200

    Using state, events and event handlers

--- expenses-project/src/components/expenseCard.jsx
+++ expenses-project/src/components/expenseCard.jsx
@@ -1,17 +1,22 @@
 import "./expenseCard.css";
+import { useState } from "react";
 import DateData from "./DateData";
 import Card from "./Card";
 
 const ExpenseCard = (props) => {
+  const [amount, setAmount] = useState(props.amount);
+
   const clickHandler = () => {
-    console.log("You clicked.");
+    setAmount(() => "0.00");
+
+    console.log(amount); //Cuando se ejecuta la función de estado, el valor no se cambia inmediatamente, si no que su cambio es postpuesto.
   };
 
   return (
     <Card className="flex expense-card">
       <DateData date={props.date} />
       <div className="expense-name">{props.title}</div>
-      <div className="expense-priec">{props.amount}</div>
+      <div className="expense-priec">{amount}</div>
       <button onClick={clickHandler}>Change</button>
     </Card>
   );
