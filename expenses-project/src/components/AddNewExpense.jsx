d2ec4233bc87692a6152700349e7bbff360e81d9
Lucas Lobato Botelho <criptoryu@gmail.com>
2023-04-19 16:10:53 +0200

    Preparing the event listeners and event handler functions to receive the event object and work with states based on the user input

--- expenses-project/src/components/AddNewExpense.jsx
+++ expenses-project/src/components/AddNewExpense.jsx
@@ -1,26 +1,28 @@
 import "./AddNewExpense.css";
 
 const AddNewExpense = () => {
+  const titleChangeHandler = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
-    <div className="new-expense">
+    <div className="flex new-expense">
       <form>
-        <div>
-          <div>
+        <div className="flex new-expense-form">
+          <div className="flex inputContainer">
             <label>Title</label>
-            <input type="text" />
+            <input type="text" onChange={titleChangeHandler} />
           </div>
-          <div>
+          <div className="flex inputContainer">
             <label>Amount</label>
             <input type="number" min="0.01" />
           </div>
-          <div>
+          <div className="flex inputContainer">
             <label>Date</label>
             <input type="date" min="2023-01-01" />
           </div>
         </div>
-        <div>
-          <button type="submit">Add</button>
-        </div>
+        <button type="submit">Add</button>
       </form>
     </div>
   );
